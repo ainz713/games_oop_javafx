5f6adfa454106cf0d5a91a75582f45d7052a8449
ainz <ainz713@gmail.com>
2021-01-28 16:09:41 +0300

    2. Сокобан. Тестовое задание. [#443604] исправил

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,43 +2,35 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        return (vertCheck(board) || horCheck(board));
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1 && (vertCheck(board, i) || horCheck(board, i))) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 
-    public static boolean vertCheck(int[][] board) {
-        boolean rsl = false;
-        int count;
+    public static boolean vertCheck(int[][] board, int column) {
+        boolean rsl = true;
         for (int i = 0; i < board.length; i++) {
-            count = 0;
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[j][i] == 1) {
-                    count++;
-                }
-                if (count == 5) {
-                    rsl = true;
-                    break;
-                }
+            if (board[i][column] != 1) {
+                rsl = false;
+                break;
             }
         }
         return rsl;
     }
 
-    public static boolean horCheck(int[][] board) {
-        boolean rsl = false;
-        int count;
+    public static boolean horCheck(int[][] board, int row) {
+        boolean rsl = true;
         for (int i = 0; i < board.length; i++) {
-            count = 0;
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 1) {
-                    count++;
-                }
-                if (count == 5) {
-                    rsl = true;
-                    break;
-                }
+            if (board[row][i] != 1) {
+                rsl = false;
+                break;
             }
         }
         return rsl;
     }
-
 }
